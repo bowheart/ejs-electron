350dac324247227761f03db1130544fd152b1b5d
bowheart <kingdomdance602@gmail.com>
2017-03-30 15:24:48 -0600

    complete refactor; code cleanup; api rework; make compatible with Electron 1.0 (breaking changes)

--- index.js
+++ index.js
@@ -1,39 +1,125 @@
+'use strict'
+
+// Define the module
+const EjsElectron = module.exports = {
+	data,
+	listen,
+	listening,
+	options,
+	stopListening
+}
+
 // Load dependencies
-var app = require('electron').app,
-    protocol = require('electron').protocol,
-    ejs = require('ejs'),
-    mime = require('mime'),
-    fs = require('fs'),
-    path = require('path'),
-    url = require('url')
+const {app, protocol} = require('electron')
+const ejs = require('ejs')
+const fs = require('fs')
+const mime = require('mime')
+const path = require('path')
+const url = require('url')
 
-// Private variables
-var userOpts = {}
+// Set up local state
+let state = {
+	data: {},
+	listening: false,
+	options: {}
+}
 
-// Helper functions
-var compileEjs = function(contentBuffer) {
-    var contentString = contentBuffer.toString(),
-        compiledEjs = ejs.render(contentString, userOpts)
+
+
+// Method API
+
+/*
+	EjsElectron.data() -- Get/set the data (context) that will be passed to `ejs.render()`.
+	Overloads:
+		ejse.data('key') -- Retrieve the value of 'key' in the current data set.
+		ejse.data('key', 'val') -- Set 'key' to 'val' in the current data set.
+		ejse.data({key: 'val'}) -- Replace the current data set with a new one containing {key: 'val'}
+*/
+function data(key, val) {
+	return updateState('data', 'ejse.data()', key, val)
+}
+
+
+/*
+	EjsElectron.listen() -- Start intercepting requests on the 'file:' protocol, looking for '.ejs' files.
+	It is not necessary to call this function up-front, as ejs-electron starts listening as soon as it's loaded.
+	Use this only to start listening again after calling EjsElectron.stopListening().
+*/
+function listen() {
+	if (state.listening) return EjsElectron // already listening; nothing to do here
+
+	protocol.interceptBufferProtocol('file', protocolListener)
+	state.listening = true
+	return EjsElectron // for chaining
+}
+
+
+/*
+	EjsElectron.listening() -- True if ejs-electron is currently intercepting requests on the 'file:' protocol.
+*/
+function listening() {
+	return state.listening
+}
+
+
+/*
+	EjsElectron.options() -- Get/set the options that will be passed to `ejs.render()`.
+	Overloads:
+		ejse.options('key') -- Retrieve the value of 'key' in the current options set.
+		ejse.options('key', 'val') -- Set 'key' to 'val' in the current options set.
+		ejse.options({key: 'val'}) -- Replace the current options set with a new one containing {key: 'val'}
+*/
+function options(key, val) {
+	return updateState('options', 'ejse.options()', key, val)
+}
+
+
+/*
+	EjsElectron.stopListening() -- Stop intercepting requests, restoring the original 'file:' protocol handler.
+*/
+function stopListening() {
+	if (!state.listening) return EjsElectron // we're not listening; nothing to stop here
+
+	protocol.uninterceptProtocol('file')
+	state.listening = false
+	return EjsElectron
+}
+
+
+
+// Helper Functions
+function compileEjs(pathname, contentBuffer) {
+	state.data.filename = pathname
+	state.data.ejse = EjsElectron
+	state.options.filename = pathname
+
+    let contentString = contentBuffer.toString()
+    let compiledEjs = ejs.render(contentString, state.data, state.options)
 
     return new Buffer(compiledEjs)
 }
 
-var protocolListener = function(request, callback) {
+
+function parsePathname(reqUrl) {
+	let parsed = url.parse(reqUrl)
+	let pathname = parsed.pathname
+
+	if (process.platform === 'win32' && !parsed.host.trim()) {
+		pathname = pathname.substr(1)
+	}
+	return pathname
+}
+
+
+function protocolListener(request, callback) {
     try {
-        var parsed = url.parse(request.url);
-        var pathname = parsed.pathname;
-        if (process.platform === 'win32' && !parsed.host.trim()) {
-            pathname = pathname.substr(1);
-        }
+		let pathname = parsePathname(request.url)
+        let fileContents = fs.readFileSync(pathname)
+        let extension = path.extname(pathname)
+        let mimeType = mime.lookup(extension)
 
-        var fileContents = fs.readFileSync(pathname),
-            extension = pathname.split('.').pop(),
-            mimeType = mime.lookup(extension)
-
-        if (extension === 'ejs') {
-            userOpts.filename = pathname
-            userOpts.ejse = this
-            fileContents = compileEjs(fileContents)
+        if (extension === '.ejs') {
+            fileContents = compileEjs(pathname, fileContents)
             mimeType = 'text/html'
         }
 
@@ -43,43 +129,27 @@ var protocolListener = function(request, callback) {
         })
 
     } catch(exception) {
-        console.error(exception);
+        console.error(exception)
         return callback(-6) // NET_ERROR(FILE_NOT_FOUND, -6)
     }
 }
 
 
-// Module setup
-var EJSE = function() {
-    var self = this
-    app.on('ready', function() {
-        self.listen()
-    })
-}
-EJSE.prototype = {
+function updateState(field, context, key, val) {
+	if (typeof key === 'string') {
+		if (typeof val === 'undefined') return state[field][key]
+		state[field][key] = val
+		return EjsElectron // for chaining
+	}
 
-    // Start intercepting requests, looking for '.ejs' files.
-    listen: function() {
-        if (!protocol) return this
-        protocol.interceptBufferProtocol('file', protocolListener.bind(this))
-        return this
-    },
-
-    // Set the options to be passed in to `ejs.render()`.
-    setOptions: function(optsIn) {
-        userOpts = optsIn || {}
-        return this
-    },
-
-    // Stop intercepting requests, restoring the original `file://` protocol handler.
-    stopListening: function() {
-        if (!protocol) return this
-
-        protocol.uninterceptProtocol('file')
-        return this
-    }
+	if (Array.isArray(key) || typeof key !== 'object') {
+		throw new TypeError(`EjsElectron Error - ${context} - Method accepts either a key and (optional) value or an object. Received ${typeof key}`)
+	}
+	state[field] = key
+	return EjsElectron // for chaining
 }
 
 
-// Expose stuff
-module.exports = new EJSE()
+
+// Get this party started
+app.on('ready', listen)
